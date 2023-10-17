682b765634b0b4860a1df78edd78d80beb600e77
hathanhbenflutter2002 <142580612+WPFHaThanhBen@users.noreply.github.com>
2023-10-17 08:40:55 +0700

    Update code version 0.0.1

---
+++ Web/WebThamMyVien/WebThamMyVien/Controllers/AccountTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebThamMyVien.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
